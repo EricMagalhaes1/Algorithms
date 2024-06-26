8ba981e6b183972d3faeb2a207cd675cdaec04f7
Eric Magalhães <kir3vv@gmail.com>
2024-06-26 12:04:38 -0300

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+public class stack{
+  public static void main(String[] args) {
+     
+    Stack<String> stack = new Stack<String>();
+
+    
+    stack.push("Minecraft");
+    stack.push("Skyrin");
+    stack.push("Doom");
+    stack.push("Borderlands 3");
+    stack.push("FFVII");
+
+    //String myFavGame = stack.pop();
+
+    System.out.println(stack.peek());
+    System.out.println(stack);
+
+    System.out.println(stack.search("Minecraft"));
+  
+  }
+}
